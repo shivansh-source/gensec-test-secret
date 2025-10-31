3d36da47dff694930a5627f08033f82d28730e4c
shivansh-source <shivanshsiddhi1234@gmail.com>
2025-10-31 22:59:04 +0530

    Identify vulnerabilities in main.go
    Exposed database password and SQL injection vulnerability found in user query.

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"os"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql" // Blank import for SQL driver
 )
 
-// Gitleaks will not find this
-var dbPassword = os.Getenv("DB_PASSWORD")
-
 func main() {
-	fmt.Println(dbPassword)
+	// VULNERABILITY 1: Hardcoded Secret (Found by Gitleaks/Trivy)
+	// The database password is exposed in the source code.
+	db, err := sql.Open("mysql", "admin:Pa$$w0rd123@tcp(127.0.0.1:3306)/users_db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		userID := r.URL.Query().Get("id")
+
+		// VULNERABILITY 2: SQL Injection (Found by Semgrep)
+		// User input is directly added to the SQL query.
+		query := "SELECT username FROM users WHERE user_id = '" + userID + "'"
+		
+		var username string
+		err := db.QueryRow(query).Scan(&username)
+		if err != nil {
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		}
+
+		fmt.Fprintf(w, "Username: %s", username)
+	})
+
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
